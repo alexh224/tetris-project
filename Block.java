cffd423a385aa6eb3f0f01eb66ec43e347ee71ca
alexh224 <ahernandez2018@student.amsacs.org>
2016-05-31 15:13:10 -0400

    Update Block.java

--- Block.java
+++ Block.java
@@ -3,19 +3,16 @@ import java.awt.Point;
 
 public class Block {
 	
-	public Point p = new Point(160,15);
-	
+	public Point p = new Point(0,0);
 	public int type;
-	
-	public boolean[][] blockCoords = {{false,false,false,false},
-								      {false,false,false,false},
-								      {false,false,false,false},
-								      {false,false,false,false}};
+	public boolean isFalling;
+	public boolean[][] blockCoords = new boolean[4][4];
 	
 	public Block(int x, int y) {
 		p.x = x;
 		p.y = y;
 		type = (int)(Math.random()*7);
+		isFalling = true;
 	}
 
 }
